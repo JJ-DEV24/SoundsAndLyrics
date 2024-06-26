605ad25b237bb0cec1aec5da6675fe78fbb10021
Jessica J <jj.dev24@gmail.com>
2024-06-26 20:36:28 +0100

    new feature: read file if lyrics exist and write to file if lyrics don't

--- main.py
+++ main.py
@@ -19,12 +19,18 @@ Pages on web application:
 - Recent searches/ history
     - List of recent searches. Includes artist's name, song title and date when searched
 
+- Lyrics page
+    - Displays artist's name, title of the song and lyrics.
+    - Provides information about the song such as release date, name of album, credits and length of song
+    - Gives the user the option to play the song if they want to sing along
+    - Highlight each word/ line of lyrics and synchronise it with the song (e.g. Spotify/ Amazon music)
 
 Colour scheme:
 
 - Pink, red, white, green & blue
-- Fun, energising,
+- Fun, colourful,
 """
+import os.path
 
 """Import templates"""
 # URL = http://127.0.0.1:8000
@@ -60,35 +66,55 @@ def home_page(request: Request):
 
 
 @app.post('/get-lyrics')
+
 async def get_lyrics(request: Request):
     user_data = await request.form()
-    len_artists_name = (user_data['artists_name'].split(' '))
-    len_title = (user_data['title'].split(' '))
-    if len(len_artists_name) and len(len_title) == 1:
-        artists_name = '-'.join(len_artists_name)
-        title = '-'.join(len_title)
-        response = requests.get(f'https://genius.com/{artists_name}-{title}-lyrics')
-        soup = BeautifulSoup(response.text, "html.parser")
-        retrieved_lyrics = soup.find_all(attrs={"data-lyrics-container": "true"})
-        answer = []
-        for lyrics in retrieved_lyrics:
-            answer.append(str(lyrics.text))
-        return answer
+    user_data_a_n = user_data['artists_name'].split(' ')
+    user_data_t = user_data['title'].split(' ')
+    artists_name = '-'.join(user_data_a_n)
+    title = '-'.join(user_data_t)
+    if os.path.isfile(f'{artists_name}-{title}-lyrics.html'):
+        with open(f'{artists_name}-{title}-lyrics.html', 'r') as f:
+            lyrics = f.read()
+        return lyrics
 
     else:
-        if len(len_artists_name) and len(len_title) >= 2:
-            artists_name = '-'.join(len_artists_name)
-            title = '-'.join(len_title)
-            response = requests.get(f'https://genius.com/{artists_name}-{title}-lyrics')
-            soup = BeautifulSoup(response.text, "html.parser")
-            retrieved_lyrics = soup.find_all(attrs={"data-lyrics-container": "true"})
-            answer = []
-            for lyrics in retrieved_lyrics:
-                answer.append(str(lyrics.text))
-            return answer
+        with open(f'{artists_name}-{title}-lyrics.html', 'w', encoding="utf-8") as f:
+
+            len_artists_name = (user_data['artists_name'].split(' '))
+            len_title = (user_data['title'].split(' '))
+            if len(len_artists_name) and len(len_title) == 1:
+                artists_name_joined = '-'.join(len_artists_name)
+                title_joined = '-'.join(len_title)
+                response = requests.get(f'https://genius.com/{artists_name_joined}-{title_joined}-lyrics')
+                soup = BeautifulSoup(response.text, "html.parser")
+                retrieved_lyrics = soup.find_all(attrs={"data-lyrics-container": "true"})
+                answer = []
+                for lyrics in retrieved_lyrics:
+                    answer.append(str(lyrics.text))
+                f.write(str(answer))
 
 
+            else:
+                if len(len_artists_name) and len(len_title) >= 2:
+                    artists_name_joined = '-'.join(len_artists_name)
+                    title_joined = '-'.join(len_title)
+                    response = requests.get(f'https://genius.com/{artists_name_joined}-{title_joined}-lyrics')
+                    soup = BeautifulSoup(response.text, "html.parser")
+                    retrieved_lyrics = soup.find_all(attrs={"data-lyrics-container": "true"})
+                    answer = []
+                    for lyrics in retrieved_lyrics:
+                        answer.append(str(lyrics.text))
+                    f.write(str(answer))
 
+        with open(f'{artists_name}-{title}-lyrics.html','r') as f:
+            lyrics = f.read()
+        return lyrics
+
+# Test: Use setUp() (instantiates a class for each test and stores it in a flie, directory or database) and tearDown()
+# (deletes/ removes the instantiated class that was saved in a file, directory or database methods to test your get
+# lyrics function.
+# use mock-ups to test URL was parsed correctly.
 
 
 
